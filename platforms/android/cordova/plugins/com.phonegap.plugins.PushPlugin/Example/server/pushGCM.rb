4f8e0ff2a624c176ca3100eb1c651671ed006d85
Sebastian Sierra <sebastian@tarmac.io>
2013-11-30 18:51:48 -0200

    Example notification

--- platforms/android/cordova/plugins/com.phonegap.plugins.PushPlugin/Example/server/pushGCM.rb
+++ platforms/android/cordova/plugins/com.phonegap.plugins.PushPlugin/Example/server/pushGCM.rb
@@ -2,8 +2,8 @@ require 'rubygems'
 require 'pushmeup'
 GCM.host = 'https://android.googleapis.com/gcm/send'
 GCM.format = :json
-GCM.key = "API_KEY_GOES_HERE"
-destination = ["REGISTRATION_ID_GOES_HERE"]
+GCM.key = "AIzaSyBlXWeIWYMkbz58sSruSU-FRB1wvzgN1Tc"
+destination = ["APA91bFFTCNemepD8amptYeBm7aAigo82UFiJvMlWL6Dgq4bT_wOPtEVqLwZtEThF1YaZw8jdc_zo6B7G5kQwgVsDeselYCcX6BtdzizNYcPLTsD6JinTqZfQiEckWK6YFMIQn43hXhe"]
 data = {:message => "PhoneGap Build rocks!", :msgcnt => "1", :soundname => "beep.wav"}
-
 GCM.send_notification( destination, data)
+
