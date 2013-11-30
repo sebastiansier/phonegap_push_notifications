209dd44d209bf3f664ac218e2b97d7d7dd2ad7b7
Sebastian Sierra <sebastian@tarmac.io>
2013-11-30 17:58:51 -0200

    Device registering on GCM

--- www/js/push_handler.js
+++ www/js/push_handler.js
@@ -1,27 +1,30 @@
-var pushNotification;
 
 function onDeviceReady() {
-  $("#app-status-ul").append('<li>deviceready event received</li>');
+  //$("#app-status-ul").append('<li>deviceready event received</li>');
+  console.log('***************** deviceready event received</li>');
+
   document.addEventListener("backbutton", function(e)
   {
-    $("#app-status-ul").append('<li>backbutton event received</li>');
-    if( $("#home").length > 0)
-    {
-      // call this to get a new token each time. don't call it to reuse existing token.
-      //pushNotification.unregister(successHandler, errorHandler);
-      e.preventDefault();
-      navigator.app.exitApp();
-    }
-    else
-    {
-      navigator.app.backHistory();
-    }
+    //$("#app-status-ul").append('<li>backbutton event received</li>');
+    //if( $("#home").length > 0)
+    //{
+    //  // call this to get a new token each time. don't call it to reuse existing token.
+    //  //pushNotification.unregister(successHandler, errorHandler);
+    //  e.preventDefault();
+    //  navigator.app.exitApp();
+    //}
+    //else
+    //{
+    //  navigator.app.backHistory();
+    //}
   }, false);
   try 
   { 
+    alert('updated pushNotification var');
     pushNotification = window.plugins.pushNotification;
-    if (device.platform == 'android' || device.platform == 'Android') {
-      $("#app-status-ul").append('<li>registering android</li>');
+    //if (device.platform == 'android' || device.platform == 'Android') {
+      alert('the device is an android');
+      //$("#app-status-ul").append('<li>registering android</li>');
       pushNotification.register(
         successHandler,
         errorHandler, {
@@ -29,18 +32,18 @@ function onDeviceReady() {
           "ecb":"onNotificationGCM"
         }
       );		// required!
-    } else {
-      $("#app-status-ul").append('<li>registering iOS</li>');
-      pushNotification.register(
-        tokenHandler,
-        errorHandler, {
-          "badge":"true",
-          "sound":"true",
-          "alert":"true",
-          "ecb":"onNotificationAPN"
-        }
-      );	// required!
-    }
+    //} else {
+    //  $("#app-status-ul").append('<li>registering iOS</li>');
+    //  pushNotification.register(
+    //    tokenHandler,
+    //    errorHandler, {
+    //      "badge":"true",
+    //      "sound":"true",
+    //      "alert":"true",
+    //      "ecb":"onNotificationAPN"
+    //    }
+    //  );	// required!
+    //}
   }
   catch(err) 
   { 
@@ -51,31 +54,31 @@ function onDeviceReady() {
 }
             
 // handle APNS notifications for iOS
-function onNotificationAPN(e) {
-  if (e.alert) {
-    $("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
-    navigator.notification.alert(e.alert);
-  }
-      
-  if (e.sound) {
-    var snd = new Media(e.sound);
-    snd.play();
-  }
-
-  if (e.badge) {
-    pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
-  }
-}
+//function onNotificationAPN(e) {
+//  if (e.alert) {
+//    $("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
+//    navigator.notification.alert(e.alert);
+//  }
+//      
+//  if (e.sound) {
+//    var snd = new Media(e.sound);
+//    snd.play();
+//  }
+//
+//  if (e.badge) {
+//    pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+//  }
+//}
             
 // handle GCM notifications for Android
 function onNotificationGCM(e) {
-  $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+  //$("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
   switch( e.event )
   {
     case 'registered':
       if ( e.regid.length > 0 )
       {
-        $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+        //$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
         // Your GCM push server needs to know the regID before it can push to this device
         // here is where you might want to send it the regID for later use.
         console.log("regID = " + e.regid);
@@ -87,46 +90,49 @@ function onNotificationGCM(e) {
       // you might want to play a sound to get the user's attention, throw up a dialog, etc.
       if (e.foreground)
       {
-        $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+        //$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
         // if the notification contains a soundname, play it.
         var my_media = new Media("/android_asset/www/"+e.soundname);
         my_media.play();
       }
       else
       {	// otherwise we were launched because the user touched a notification in the notification tray.
-        if (e.coldstart)
-          $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
-        else
-          $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+        //if (e.coldstart)
+          //$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+        //else
+          //$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
       }
 						
-      $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
-      $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+      //$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+      //$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
       break;
         
     case 'error':
-      $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+      //$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
       break;
         
     default:
-      $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+      //$("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
       break;
   }
 }
             
 function tokenHandler (result) {
-  $("#app-status-ul").append('<li>token: '+ result +'</li>');
+  //$("#app-status-ul").append('<li>token: '+ result +'</li>');
   // Your iOS push server needs to know the token before it can push to this device
   // here is where you might want to send it the token for later use.
 }
 
 function successHandler (result) {
-  $("#app-status-ul").append('<li>success:'+ result +'</li>');
+  //$("#app-status-ul").append('<li>success:'+ result +'</li>');
+  console.log(result);
+  alert(result);
 }
 
 function errorHandler (error) {
-  $("#app-status-ul").append('<li>error:'+ error +'</li>');
+  //$("#app-status-ul").append('<li>error:'+ error +'</li>');
+  console.log(error);
+  alert(error);
 }
           
-document.addEventListener('deviceready', onDeviceReady, true);
 
